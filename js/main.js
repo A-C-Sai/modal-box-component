f6945179f1e9d7625ef49eaea3e64300d0e501cf
A-C-Sai <64731868+A-C-Sai@users.noreply.github.com>
2022-08-06 02:27:23 +1000

    Finished Challenge

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 // Start writing JavaScript here!
+const body = document.body;
+const openModalBtn = document.querySelector(".jsModalButton");
+const closeModalBtn = document.querySelector(".jsModalClose");
+
+openModalBtn.addEventListener("click", (e) => {
+  body.classList.add("modal-is-open");
+});
+
+closeModalBtn.addEventListener("click", function () {
+  body.classList.remove("modal-is-open");
+});
